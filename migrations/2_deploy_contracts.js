7fdd86d5a72f951b850dafaad747612fd53fb497
Andre Tan <tanandre93@gmail.com>
2021-01-23 18:18:44 +0800

    [andre] remove truffle box contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+const Game = artifacts.require("Game");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+module.exports = function (deployer) {
+  deployer.deploy(Game);
 };
