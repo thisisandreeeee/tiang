e3aca0403b7652f530d3376c8be7b13037d46eed
Andre Tan <tanandre93@gmail.com>
2021-01-25 00:22:41 +0800

    [andre] rename variables

--- contracts/InBetween.sol
+++ contracts/InBetween.sol
@@ -9,13 +9,14 @@ contract InBetween is Ownable {
     mapping(address => uint256) private _bets;
 
     function joinGame() external payable {
-        require(_bets[msg.sender] == 0, "Player is already in game");
-        require(msg.value >= ante, "Sent amount less than ante");
+        require(_bets[msg.sender] == 0, "player is already in game");
+        require(msg.value >= ante, "sent amount less than ante");
 
+        // TODO: check whether this is net of gas fee
         _bets[msg.sender] += msg.value;
         // TODO: refund excess ante
 
-        // give cards
+        // draw 2 pseudorandom cards
 
         // put player in queue
         // https://ethereum.stackexchange.com/questions/63708/how-to-properly-delete-the-first-element-in-an-array
@@ -23,7 +24,7 @@ contract InBetween is Ownable {
 
     function viewStake() external view returns (uint256) {
         uint256 stake = _bets[msg.sender];
-        require(stake > 0, "Player is not in game");
+        require(stake > 0, "player must be in game to view stake");
         return stake;
     }
 }
