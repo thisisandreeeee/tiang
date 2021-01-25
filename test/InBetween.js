1238196c2bb0adac9b5b59d461d37ffb61ea9ebb
Andre Tan <tanandre93@gmail.com>
2021-01-25 23:02:45 +0800

    [andre] add test stubs

--- test/InBetween.js
+++ test/InBetween.js
@@ -5,26 +5,33 @@ const { expect } = require('chai');
 const InBetween = contract.fromArtifact("InBetween");
 
 describe('InBetween', function () {
+  let owner, player;
+  let inBetween;
+
   beforeEach(async function () {
+    [owner, player] = accounts;
+    inBetween = await InBetween.new({ from: owner });
   });
 
   it("reverts when joining with insufficient funds", async function () {
-    const [owner, player] = accounts;
-    inBetween = await InBetween.new({ from: owner });
     await expectRevert(inBetween.joinGame({ from: player, value: 5 }), 'sent amount less than ante');
   });
 
   it("reverts when player is already in game", async function () {
-    const [owner, player] = accounts;
-    inBetween = await InBetween.new({ from: owner });
     await inBetween.joinGame({ from: player, value: 100 });
     await expectRevert(inBetween.joinGame({ from: player, value: 100 }), 'player is already in game');
   });
 
-  it("returns the correct stake when joining a game", async function () {
-    const [owner, player] = accounts;
-    inBetween = await InBetween.new({ from: owner });
+  it("ante deposited when joining game is correct", async function () {
     await inBetween.joinGame({ from: player, value: 100 });
-    expect(await inBetween.viewStake({ from: player })).to.be.equal(100);
+    let stake = await inBetween.viewStake({ from: player });
+    expect(stake.toNumber()).to.equal(100);
   });
+
+  xit("excess ante is refunded to the player", async function () { });
+
+  xit("player gets two cards when joining game", async function () { });
+
+  xit("revert when drawing opening cards again", async function () { });
+  xit("revert when drawing final card without opening cards", async function () { });
 });
---
+++ test/Queue.js
@@ -0,0 +1,9 @@
+const { accounts, contract } = require('@openzeppelin/test-environment');
+const { expect } = require('chai');
+
+const Queue = contract.fromArtifact("Queue");
+
+describe('Queue', function () {
+  beforeEach(async function () {
+  });
+});
