d710d2bd90ae29f0ba49ea98a80daa9f0838bda6
Andre Tan <tanandre93@gmail.com>
2021-02-11 00:22:57 +0800

    [andre] skeleton app

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -28,101 +28,85 @@ export default ({ drizzle, drizzleState }) => {
       </div>
 
       <div className="section">
-        <h2>SimpleStorage</h2>
-        <p>
-          This shows a simple ContractData component with no arguments, along
-          with a form to set its value.
-        </p>
-        <p>
-          <strong>Stored Value: </strong>
-          <ContractData
+        <h2>InBetween</h2>
+        <ul>
+          <li>
+            <strong>Next player: </strong>
+            <ContractData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="InBetween"
+              method="nextPlayer"
+            />
+          </li>
+          <li>
+            <strong>Pot: </strong>
+            <ContractData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="InBetween"
+              method="pot"
+            />
+          </li>
+          <li>
+            <strong>Escrow: </strong>
+            <ContractData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="InBetween"
+              method="payments"
+              methodArgs={[drizzleState.accounts[0]]}
+            />
+          </li>
+          <li>
+            <strong>Cards: </strong>
+            <ContractData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="InBetween"
+              method="viewCards"
+            />
+          </li>
+          <li>
+            <strong>Result: </strong>
+            <ContractData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="InBetween"
+              method="viewResult"
+            />
+          </li>
+        </ul>
+        <div>
+          <strong>Join game: </strong>
+          <ContractForm
             drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="SimpleStorage"
-            method="storedData"
+            contract="InBetween"
+            method="joinGame"
+            sendArgs={{ from: drizzleState.accounts[0], value: 100, gas: 6000000 }}
           />
-        </p>
-        <ContractForm drizzle={drizzle} contract="SimpleStorage" method="set" />
-      </div>
-
-      <div className="section">
-        <h2>TutorialToken</h2>
-        <p>
-          Here we have a form with custom, friendly labels. Also note the token
-          symbol will not display a loading indicator. We've suppressed it with
-          the <code>hideIndicator</code> prop because we know this variable is
-          constant.
-        </p>
-        <p>
-          <strong>Total Supply: </strong>
-          <ContractData
+          <strong>Bet: </strong>
+          <ContractForm
             drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="TutorialToken"
-            method="totalSupply"
-            methodArgs={[{ from: drizzleState.accounts[0] }]}
-          />{" "}
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="TutorialToken"
-            method="symbol"
-            hideIndicator
+            contract="InBetween"
+            method="bet"
+            sendArgs={{ from: drizzleState.accounts[0], value: 200, gas: 6000000 }}
           />
-        </p>
-        <p>
-          <strong>My Balance: </strong>
-          <ContractData
+          <strong>Withdraw: </strong>
+          <ContractForm
             drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="TutorialToken"
-            method="balanceOf"
-            methodArgs={[drizzleState.accounts[0]]}
+            contract="InBetween"
+            method="withdraw"
+            sendArgs={{ from: drizzleState.accounts[0], gas: 6000000 }}
           />
-        </p>
-        <h3>Send Tokens</h3>
-        <ContractForm
-          drizzle={drizzle}
-          contract="TutorialToken"
-          method="transfer"
-          labels={["To Address", "Amount to Send"]}
-        />
-      </div>
-
-      <div className="section">
-        <h2>ComplexStorage</h2>
-        <p>
-          Finally this contract shows data types with additional considerations.
-          Note in the code the strings below are converted from bytes to UTF-8
-          strings and the device data struct is iterated as a list.
-        </p>
-        <p>
-          <strong>String 1: </strong>
-          <ContractData
+          <strong>Reset: </strong>
+          <ContractForm
             drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="ComplexStorage"
-            method="string1"
-            toUtf8
+            contract="InBetween"
+            method="reset"
+            sendArgs={{ from: drizzleState.accounts[0], gas: 6000000 }}
           />
-        </p>
-        <p>
-          <strong>String 2: </strong>
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="ComplexStorage"
-            method="string2"
-            toUtf8
-          />
-        </p>
-        <strong>Single Device Data: </strong>
-        <ContractData
-          drizzle={drizzle}
-          drizzleState={drizzleState}
-          contract="ComplexStorage"
-          method="singleDD"
-        />
+        </div>
       </div>
     </div>
   );
