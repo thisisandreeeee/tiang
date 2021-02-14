a47d2335bb9d816f500e8e80e48b6e7d3f095db4
Andre Tan <tanandre93@gmail.com>
2021-02-14 23:57:49 +0800

    [andre] create queue interface

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -4,7 +4,19 @@ pragma solidity 0.7.6;
 import "@openzeppelin/contracts/utils/SafeCast.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract Queue {
+interface Queue {
+    function push(address data) external;
+
+    function pop() external returns (address);
+
+    function head() external view returns (address);
+
+    function tail() external view returns (address);
+
+    function length() external view returns (uint256);
+}
+
+contract FifoQueue is Queue {
     using SafeMath for uint128;
     using SafeCast for uint256;
 
@@ -17,28 +29,28 @@ contract Queue {
         _;
     }
 
-    function push(address data) public {
+    function push(address data) public override {
         // TODO: check for duplicates before push
         queue[last] = data;
         last = last.add(1).toUint128();
     }
 
-    function pop() public notEmpty returns (address) {
+    function pop() public override notEmpty returns (address) {
         address data = queue[first];
         delete queue[first];
         first = first.add(1).toUint128();
         return data;
     }
 
-    function head() public view notEmpty returns (address) {
+    function head() public view override notEmpty returns (address) {
         return queue[first];
     }
 
-    function tail() public view notEmpty returns (address) {
+    function tail() public view override notEmpty returns (address) {
         return queue[last - 1];
     }
 
-    function length() public view returns (uint256) {
+    function length() public view override returns (uint256) {
         return last - first;
     }
 }
